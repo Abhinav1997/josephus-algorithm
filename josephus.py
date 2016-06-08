4610541deeecfcef885d6f23234ff3750ae42382
Abhinav Jhanwar <ajhanwar97@gmail.com>
2016-06-09 00:02:23 +0530

    Add support for frequency and survivor number

--- josephus.py
+++ josephus.py
@@ -1,21 +1,43 @@
-def onezero(binary) :
+import sys
 
-	new_binary = ""
+def eliminate(nPeople, kill, nSurvivor = 1) :
+	left = 0
+	while len(nPeople) > nSurvivor :
+		num = kill - left
 
-	for bin in binary:
-		if bin == "0" :
-			new_binary += "1"
-		else :
-			new_binary += "0"
-	
-	return new_binary
+		if(num > len(nPeople)) :
+			while num > len(nPeople) :
+				num = num - len(nPeople)
 
-bin = "{0:b}"
+		while num <= len(nPeople) :
+			nPeople[num - 1] = 0
+			num = num + kill
+		
+		try :
+			left = len(nPeople[(((len(nPeople) - 1) - nPeople[::-1].index(0)) + 1):])
+		except ValueError :
+			pass
 
-initial = input('Enter the initial number of people : ')
+		nPeople = list(filter((0).__ne__, nPeople))
+	return nPeople
 
-init = bin.format(int(initial))
-sub = onezero(init)
-final = int(init, 2) - int(sub, 2)
+people = input('Enter the initial number of people : ')
+killfreq = input('Enter the person number to be killed : ')
+survivor = input('Enter number of survivors : ')
 
-print("The final person alive would be number " + str(final))
+if int(survivor) == 0 :
+	print("\n\nEveryone is dead!")
+	sys.exit()
+elif int(survivor) < 0 :
+	survivor = 1
+
+peopleList = [int(i+1) for i in range (int(people))]
+
+survivorList = eliminate(peopleList, int(killfreq), int(survivor))
+
+if len(survivorList) == 1 :
+	print("\n\nThe survivor is number " + str(survivorList[0]))
+else :
+	print("\n\nThe survivor numbers are : ")
+	for i in survivorList :
+		print(i, end = " ")
