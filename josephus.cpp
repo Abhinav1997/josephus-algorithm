d34c53988c88f981658890a002184e2719417f6c
Abhinav Jhanwar <ajhanwar97@gmail.com>
2016-06-09 19:04:26 +0530

    cpp: Move elimination code to separate function

--- josephus.cpp
+++ josephus.cpp
@@ -2,9 +2,37 @@
 #include <vector>
 #include <algorithm>
 
+std::vector<int> eliminate(std::vector<int> nPeople, int kill, int nSurvivor) {
+    unsigned int left = 0, num;
+    while(nPeople.size() > nSurvivor) {
+        num = kill - left;
+
+        if(num > nPeople.size()) {
+            while(num > nPeople.size()) {
+                num = num - nPeople.size();
+            }
+        }
+
+        while(num <= nPeople.size()) {
+            nPeople[num - 1] = 0;
+            num = num + kill;
+        }
+
+        for(int j = nPeople.size() - 1; j >= 0; j--) {
+            if (nPeople[j] == 0) {
+                left = nPeople.size() - (j + 1);
+                break;
+            }
+        }
+
+        nPeople.erase(std::remove(nPeople.begin(), nPeople.end(), 0), nPeople.end());
+    }
+
+    return nPeople;
+}
+
 int main() {
     unsigned int people, killfreq, survivor;
-    unsigned int left = 0, num;
 
     std::cout << "Enter the initial number of people : ";
     std::cin >> people;
@@ -20,43 +48,21 @@ int main() {
         survivor = 1;
     }
 
-    std::vector<int> peopleList;
+    std::vector<int> peopleList, survivorList;
     peopleList.reserve(people);
 
     for(unsigned int i = 1; i <= people; ++i) {
         peopleList.push_back(i);
     }
 
-    while(peopleList.size() > survivor) {
-            num = killfreq - left;
-
-            if(num > peopleList.size()) {
-                while(num > peopleList.size()) {
-                    num = num - peopleList.size();
-                }
-            }
-
-            while(num <= peopleList.size()) {
-                peopleList[num - 1] = 0;
-                num = num + killfreq;
-            }
-
-            for(int j = peopleList.size() - 1; j >= 0; j--) {
-                    if (peopleList[j] == 0) {
-                        left = peopleList.size() - (j + 1);
-                        break;
-                    }
-            }
-
-            peopleList.erase(std::remove(peopleList.begin(), peopleList.end(), 0), peopleList.end());
-    }
+    survivorList = eliminate(peopleList, killfreq, survivor);
 
     if(survivor == 1) {
-        std::cout << "\nThe survivor is number " << peopleList[0];
+        std::cout << "\nThe survivor is number " << survivorList[0];
     } else {
         std::cout << "\nThe survivors are numbers ";
-        for(unsigned int k = 0; k < peopleList.size(); ++k) {
-            std::cout << peopleList[k] << " ";
+        for(unsigned int k = 0; k < survivorList.size(); ++k) {
+            std::cout << survivorList[k] << " ";
         }
     }
 
